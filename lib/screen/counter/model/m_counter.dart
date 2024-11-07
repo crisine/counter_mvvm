fa0c8f2c8936c80d0eadbd400514fd3054d8bfc3
misung <crisine@knou.ac.kr>
2024-11-08 00:08:47 +0900

    refactor: MVVM 패턴에서 Model 의 불변성을 지키기 위해 Model 과 ViewModel 수정

--- lib/screen/counter/model/m_counter.dart
+++ lib/screen/counter/model/m_counter.dart
@@ -1,11 +1,23 @@
 enum CountMode { plus, minus }
 
 class CounterModel {
-  int count;
-  CountMode countMode;
+  // 외부에서 실수로라도 변경할 수 있는 여지를 배제
+  final int count;
+  final CountMode countMode;
 
-  CounterModel({
+  const CounterModel({
     required this.count,
     required this.countMode,
   });
+
+  // 새로운 상태를 반환하도록 함
+  CounterModel copyWith({
+    int? count,
+    CountMode? countMode,
+  }) {
+    return CounterModel(
+      count: count ?? this.count,
+      countMode: countMode ?? this.countMode,
+    );
+  }
 }
--- lib/screen/counter/view_model/vm_counter.dart
+++ lib/screen/counter/view_model/vm_counter.dart
@@ -2,27 +2,26 @@ import 'package:counter_mvvm/screen/counter/model/m_counter.dart';
 import 'package:flutter/material.dart';
 
 class CounterViewModel extends ChangeNotifier {
-  late final CounterModel _counterModel; // CounterModel 은 갈아끼울 이유가 없으므로 final
+  late CounterModel _counterModel; // late final 로 주게 되면 새 상태 주입 불가해짐.
 
-  CounterViewModel() {
-    _counterModel = CounterModel(count: 0, countMode: CountMode.plus);
-  }
+  CounterViewModel()
+      : _counterModel = const CounterModel(count: 0, countMode: CountMode.plus);
 
   int get count => _counterModel.count;
   CountMode get countMode => _counterModel.countMode;
 
   void changeCountMode(CountMode countMode) {
-    _counterModel.countMode = countMode;
+    _counterModel = _counterModel.copyWith(countMode: countMode);
     notifyListeners(); // 상태 변경 알리기
   }
 
   void addCount() {
-    switch (_counterModel.countMode) {
-      case CountMode.plus:
-        _counterModel.count += 1;
-      case CountMode.minus:
-        _counterModel.count -= 1;
-    }
+    final newCount = _counterModel.countMode == CountMode.plus
+        ? _counterModel.count + 1
+        : _counterModel.count - 1;
+
+    // 새로운 상태를 주입함으로서 Model을 변경
+    _counterModel = _counterModel.copyWith(count: newCount);
     notifyListeners(); // 상태 변경 알리기
   }
 }
